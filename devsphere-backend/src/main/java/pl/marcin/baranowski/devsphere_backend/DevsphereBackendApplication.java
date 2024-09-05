a145920a63a6cab465c48c914c3b65e46081962e
Marcin Baranowski <mbaranowski57@gmail.com>
2024-09-05 15:27:59 +0200

    initial commit

---
+++ devsphere-backend/src/main/java/pl/marcin/baranowski/devsphere_backend/DevsphereBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.marcin.baranowski.devsphere_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsphereBackendApplication.class, args);
+	}
+
+}
---
+++ devsphere-backend/src/test/java/pl/marcin/baranowski/devsphere_backend/DevsphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcin.baranowski.devsphere_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
