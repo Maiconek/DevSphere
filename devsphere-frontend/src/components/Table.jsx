ae1f699da28e49bc0aca9f3421245505c03ca874
Marcin Baranowski <mbaranowski57@gmail.com>
2024-10-07 13:01:21 +0200

    moved projects table to a single component

---
+++ devsphere-frontend/src/components/Table.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Table = (props) => {
+    return (
+        <table className="table table-hover w-75">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Link</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.projects.map((item, index) => (
+                    <tr key={item.id}>
+                        <th scope="row">{index + 1}</th>
+                        <td>{item.title}</td>
+                        <td><img src={item.imageUrl} className="image-table" alt="image"/></td>
+                        <td>{item.link}</td>
+                            <td>
+                                <Link to={`/project/${item.id}`}>
+                                    <button type="button" className="btn btn-primary me-3">Visit</button>
+                                </Link>
+                                <Link to={`/project-form/${item.id}`}>
+                                    <button type="button" className="btn btn-success me-3">Edit</button>
+                                </Link>
+                                    <button type="button" className="btn btn-danger me-3" onClick={() => props.delete(item.id)}>Delete</button>
+                             </td>
+                    </tr>
+                ))}    
+            </tbody>
+        </table>
+    )
+}
+
+export default Table;
--- devsphere-frontend/src/pages/UserPage.jsx
+++ devsphere-frontend/src/pages/UserPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Link, useParams } from "react-router-dom";
+import Table from "../components/Table";
 
 const UserPage = () => {
     const [searchedUser, setSearchedUser] = useState('')
@@ -76,36 +77,10 @@ const UserPage = () => {
                     
                     
                     <h2>Your projects:</h2>
-                    <table className="table table-hover w-75">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Title</th>
-                                <th scope="col">Image</th>
-                                <th scope="col">Link</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {userProjects.map((item, index) => (
-                            <tr key={item.id}>
-                                <th scope="row">{index + 1}</th>
-                                <td>{item.title}</td>
-                                <td><img src={item.imageUrl} className="image-table" alt="image"/></td>
-                                <td>{item.link}</td>
-                                <td>
-                                    <Link to={`/project/${item.id}`}>
-                                        <button type="button" className="btn btn-primary me-3">Visit</button>
-                                    </Link>
-                                    <Link to={`/project-form/${item.id}`}>
-                                        <button type="button" className="btn btn-success me-3">Edit</button>
-                                    </Link>
-                                    <button type="button" className="btn btn-danger me-3" onClick={() => deleteProject(item.id)}>Delete</button>
-                                </td>
-                            </tr>
-                        ))}    
-                        </tbody>
-                    </table>
+                    <Table 
+                        projects={userProjects}
+                        delete={deleteProject}    
+                    />
                 </div>
                 :
                 <></>
