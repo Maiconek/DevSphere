e673d9f72a1d67f99460288aa515dbef7c09d43c
Marcin Baranowski <mbaranowski57@gmail.com>
2024-10-14 00:46:42 +0200

    added section component

--- devsphere-frontend/src/components/Section.jsx
+++ devsphere-frontend/src/components/Section.jsx
@@ -10,7 +10,7 @@ const Section = () => {
         <section className="py-5 text-center container">
             <div className="row py-lg-5">
                 <div className="col-lg-6 col-md-8 mx-auto">
-                    <h1 className="fw-light">Album example</h1>
+                    <h1 className="fw-light">Welcome to DevSphere!</h1>
                     <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
                     <p>
                         <Link to='/project-form'><button type="button" className="btn btn-success my-2 me-2">Add your project</button></Link>
